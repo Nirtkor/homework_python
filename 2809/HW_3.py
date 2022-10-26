c458d1f37d55f59938c8587f3261bba502af979f
Nirtkor <itnirtkor@gmail.com>
2022-10-26 09:41:32 +0300

    hw_3 flake8 fix

--- 2809/HW_3.py
+++ 2809/HW_3.py
@@ -3,7 +3,11 @@ from datetime import datetime, timedelta
 
 def captain_records(entries, first_date):
     file = open('Captain_diary', 'w')
-    for i in range(len(entries)):
-        file.write((str(((datetime.strptime(first_date, "%d-%m-%Y")) + timedelta(days = i)))[:11]) \
-            + str(entries[i]) + "\n")
+    try:
+        for i in range(len(entries)):
+            file.write((str(((datetime.strptime(first_date, "%d-%m-%Y"))
+                        + timedelta(days=i)))[:11])
+                        + str(entries[i]) + "\n")
+    finally:
+        file.close()
     return "Captain's diary is done"
