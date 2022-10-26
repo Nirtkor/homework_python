5ec0bc6cfb0f612d90f30212f8b0b03c1bcd8d2a
Nirtkor <itnirtkor@gmail.com>
2022-10-26 09:49:47 +0300

    hw_3 fix flake8 (2)

--- 2809/HW_3.py
+++ 2809/HW_3.py
@@ -5,9 +5,12 @@ def captain_records(entries, first_date):
     file = open('Captain_diary', 'w')
     try:
         for i in range(len(entries)):
-            file.write((str(((datetime.strptime(first_date, "%d-%m-%Y"))
-                        + timedelta(days=i)))[:11])
-                        + str(entries[i]) + "\n")
+            file.write(
+                (str((datetime.strptime(first_date, "%d-%m-%Y")) + timedelta(days=i)))[:11]
+                        + str(entries[i]) + "\n"
+            )
+    except:
+        return "Something is wrong with the records, try again with different data"
     finally:
         file.close()
     return "Captain's diary is done"
