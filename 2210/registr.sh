cecfea3893a99361ac2d3f642fa79c38c4e5bbc9
Nirtkor <itnirtkor@gmail.com>
2022-11-02 01:54:28 +0300

    hw bash 6

---
+++ 2210/registr.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+exec 2> log_error_registration
+database=./database.txt
+
+compliance_flag=0
+echo -n "Введите логин: "
+read login
+if [ -z $login ]
+    then
+    compliance_flag=1
+fi
+if [ -f $database ] && [ $compliance_flag == 0 ]
+then
+    while read line
+        do
+        if [[ $login == $line ]]
+            then
+            compliance_flag=1
+        fi
+    done < database
+fi
+
+if [[ $compliance_flag == 1 ]]
+    then
+    echo "Вы ничего не ввели, либо данный логин занят другим пользователем"
+elif [[ $compliance_flag == 0 ]]
+    then
+    echo "Введите пароль:"
+    read -s password
+
+fi
+    pass=$(echo -n $password | sha512sum)
+    echo $login $pass >> $database
+    echo "Вы зарегистрировались"
